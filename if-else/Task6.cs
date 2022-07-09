0e80f239ccef543eebf4096fd9b86fcd5f7c7f6d
AlexSiva <108282268+AlexSiva@users.noreply.github.com>
2022-07-09 18:37:33 +0300

    Add files via upload

---
+++ if-else/Task6.cs
@@ -0,0 +1,33 @@
+﻿namespace IfStatements
+{
+    public static class Task6
+    {
+        public static int DoSomething(int i)
+        {
+            int result = 0;
+
+            if (i < -8)
+            {
+                result = i * i;
+            }
+            else if (i >= 7)
+            {
+                return i * 2;
+            }
+            else if (i <= -2)
+            {
+                return i * 3;
+            }
+            else if (i > 3)
+            {
+                return i * (i - 1) * (-1);
+            }
+            else
+            {
+                return (i * 2) + (i * i);
+            }
+
+            return result;
+        }
+    }
+}
