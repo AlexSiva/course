5a1cea5b25dfb211216268e57644315e4805208a
AlexSiva <108282268+AlexSiva@users.noreply.github.com>
2022-07-09 18:48:20 +0300

    Add files via upload

---
+++ gcd/IntegerExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GcdTask
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue]  by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int FindGcd(int a, int b)
+        {
+            if (a == int.MinValue || b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException($"Number cannot be {int.MinValue}.");
+            }
+
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a < 0)
+            {
+                a *= -1;
+            }
+            else if (b < 0)
+            {
+                b *= -1;
+            }
+            else if (b < 0 && a < 0)
+            {
+                b *= -1;
+                a *= -1;
+            }
+
+            while (a != b && a > 0 && b > 0)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+
+            int k = a > b ? a : b;
+            return k;
+        }
+    }
+}
