314202b13f3e5ef39464ccdf4b64095075f69647
AlexSiva <108282268+AlexSiva@users.noreply.github.com>
2022-07-09 19:02:13 +0300

    Add files via upload

---
+++ all-strings/CharsCounter.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace LookingForChars
+{
+    public static class CharsCounter
+    {
+        /// <summary>
+        /// Searches a string for all characters that are in <see cref="Array" />, and returns the number of occurrences of all characters.
+        /// </summary>
+        /// <param name="str">String to search.</param>
+        /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
+        /// <returns>The number of occurrences of all characters.</returns>
+        public static int GetCharsCount(string? str, char[]? chars)
+        {
+            int count = 0;
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (chars is null)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (chars[i] == str[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Searches a string for all characters that are in <see cref="Array" />, and returns the number of occurrences of all characters within the range of elements in the <see cref="string"/> that starts at the specified index and ends at the specified index.
+        /// </summary>
+        /// <param name="str">String to search.</param>
+        /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
+        /// <param name="startIndex">A zero-based starting index of the search.</param>
+        /// <param name="endIndex">A zero-based ending index of the search.</param>
+        /// <returns>The number of occurrences of all characters within the specified range of elements in the <see cref="string"/>.</returns>
+        public static int GetCharsCount(string? str, char[]? chars, int startIndex, int endIndex)
+        {
+            int count = 0, i = 0, j = startIndex;
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (chars is null)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (startIndex > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (endIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+            }
+
+            if (endIndex > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+            }
+
+            if (startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            while (i < chars.Length)
+            {
+                while (j <= endIndex)
+                {
+                    if (chars[i] == str[j])
+                    {
+                        count++;
+                    }
+
+                    j++;
+                }
+
+                j = startIndex;
+                i++;
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Searches a string for a limited number of characters that are in <see cref="Array" />, and returns the number of occurrences of all characters within the range of elements in the <see cref="string"/> that starts at the specified index and ends at the specified index.
+        /// </summary>
+        /// <param name="str">String to search.</param>
+        /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
+        /// <param name="startIndex">A zero-based starting index of the search.</param>
+        /// <param name="endIndex">A zero-based ending index of the search.</param>
+        /// <param name="limit">A maximum number of characters to search.</param>
+        /// <returns>The limited number of occurrences of characters to search for within the specified range of elements in the <see cref="string"/>.</returns>
+        public static int GetCharsCount(string? str, char[]? chars, int startIndex, int endIndex, int limit)
+        {
+            int count = 0, i = 0, j = startIndex;
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (chars is null)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (startIndex > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (endIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+            }
+
+            if (endIndex > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+            }
+
+            if (startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (limit < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(limit));
+            }
+
+            do
+            {
+                do
+                {
+                    if (chars[i] == str[j])
+                    {
+                        count++;
+                    }
+
+                    j++;
+                }
+                while (j <= endIndex);
+                j = startIndex;
+                i++;
+            }
+            while (i < chars.Length);
+
+            if (count >= limit)
+            {
+                return limit;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ all-strings/StringExtension.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ShuffleCharacters
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Shuffles characters in source string according some rule.
+        /// </summary>
+        /// <param name="source">The source string.</param>
+        /// <param name="count">The count of iterations.</param>
+        /// <returns>Result string.</returns>
+        /// <exception cref="ArgumentNullException">Source string is null.</exception>
+        /// <exception cref="ArgumentException">Source string is empty or a white space.</exception>
+        /// <exception cref="ArgumentException">Count of iterations is less than 0.</exception>
+        public static string ShuffleChars(string? source, int count)
+        {
+            string odd = string.Empty, even = string.Empty, k;
+            int z = 0;
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (source.Replace(" ", string.Empty, StringComparison.Ordinal) == odd)
+            {
+                throw new ArgumentException("smth");
+            }
+
+            if (source.Replace("  \t\n  \t \r", string.Empty, StringComparison.Ordinal) == odd)
+            {
+                throw new ArgumentException("smth");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException("mda");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentException("smth");
+            }
+
+            string str = source;
+            for (int i = count; i > 0; i--)
+            {
+                for (int j = 0; j < source.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        k = even;
+                        even = string.Concat(k, source[j].ToString());
+                    }
+                    else
+                    {
+                        k = odd;
+                        odd = string.Concat(k, source[j].ToString());
+                    }
+                }
+
+                source = string.Concat(even, odd);
+                even = string.Empty;
+                odd = string.Empty;
+                z++;
+                if (source == str)
+                {
+                    i = count % z;
+                    i++;
+                }
+            }
+
+            return source;
+        }
+    }
+}
